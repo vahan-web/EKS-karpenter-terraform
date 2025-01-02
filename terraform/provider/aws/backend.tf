362bd8f5daf8dd31912fec76654028a97eae3688
vahan-web <78244155+vahan-web@users.noreply.github.com>
2025-01-02 22:21:54 +0400

    Update backend.tf

--- terraform/provider/aws/backend.tf
+++ terraform/provider/aws/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket               = "eks-project-terraform-state"
+    bucket               = "project-terraform-state"
     key                  = "terraform.tfstate"
     region               = "us-east-1"
     encrypt              = true
